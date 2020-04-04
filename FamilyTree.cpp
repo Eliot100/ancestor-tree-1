71680e6910ace0b18827758d4446b211c9e0f005
Eliot100 <46245107+Eliot100@users.noreply.github.com>
2020-04-04 17:34:00 +0300

    Update FamilyTree.cpp

--- FamilyTree.cpp
+++ FamilyTree.cpp
@@ -19,9 +19,8 @@ namespace family {
 		node root;
 	
 		public: 
-			Tree (string rootName) {
-			root:	(rootName);
-				
+			Tree (string rootName): root(*(new node(rootName))) {
+					
 			}
 			
 			Tree& addFather (string sonName, string fatherName){
