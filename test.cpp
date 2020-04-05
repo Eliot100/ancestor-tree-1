017c7d7453f3e83d580804c74a6b11c1ee63f838
talbg <57101649+talbg@users.noreply.github.com>
2020-04-05 20:01:10 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -11,12 +11,12 @@ using namespace family;
  
 TEST_CASE(" first case") {
 	family::Tree T ("Yosef"); 
-	T.addFather("Yosef", "Yaakov") 
-	 .addMother("Yosef", "Rachel")   
-	 .addFather("Yaakov", "Isaac")
-	 .addMother("Yaakov", "Rivka")
-	 .addFather("Isaac", "Avraham")
-	 .addFather("Avraham", "Terah");
+	T.addFather("Yosef", "Yaakov"); 
+// 	 .addMother("Yosef", "Rachel")   
+// 	 .addFather("Yaakov", "Isaac")
+// 	 .addMother("Yaakov", "Rivka")
+// 	 .addFather("Isaac", "Avraham")
+// 	 .addFather("Avraham", "Terah");
 	 
 	//for (int i = 0; i<100; i++)
 		CHECK( T.relation("Yaakov") == "father" );
