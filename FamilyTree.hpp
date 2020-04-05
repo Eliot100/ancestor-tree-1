48814159ec89176d1cc7d8321b5a29293fe78736
Eliot100 <46245107+Eliot100@users.noreply.github.com>
2020-04-05 22:21:15 +0300

    Add files via upload

--- FamilyTree.hpp
+++ FamilyTree.hpp
@@ -1,11 +1,4 @@
-
-#pragma once
 #include <iostream>
-using namespace std;
-#include <stdio.h>
-#include <string>
-#include <cmath>
-#include <cassert>
 
 namespace family {
 	
@@ -27,8 +20,8 @@ namespace family {
 		public:
 			family::Node root;
 			Tree (const std::string rootName);
-			family::Tree& family::Tree:: addFather (const std::string sonName, const std::string fatherName);
-			family::Tree& family::Tree:: addMother (const std::string sonName, const std::string motherName);
+			Tree& addFather (const std::string sonName, const std::string fatherName);
+			Tree& addMother (const std::string sonName, const std::string motherName);
 			std::string find (const std::string relation2Root);
 			void display ();
 			Tree& remove (const std::string name) ;
