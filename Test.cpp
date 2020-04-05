a9c8851c6ef05a11c312335600ce0c669915172e
Eliot100 <46245107+Eliot100@users.noreply.github.com>
2020-04-05 22:47:37 +0300

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -1,5 +1,5 @@
 #include "doctest.h"
-// #include "FamilyTree.hpp"
+#include "FamilyTree.hpp"
 // #include <string>
 // #include <iostream>;
 
@@ -9,15 +9,15 @@
  */
  
 TEST_CASE(" first case ") {
-// 	family::Tree T ("Yosef"); 
-//	T.addFather("Yosef", "Yaakov"); 
-// 	 .addMother("Yosef", "Rachel")   
-// 	 .addFather("Yaakov", "Isaac")
-// 	 .addMother("Yaakov", "Rivka")
-// 	 .addFather("Isaac", "Avraham")
-// 	 .addFather("Avraham", "Terah");
-//	 
-//	for (int i = 0; i<100; i++)
-// 		CHECK( T.relation("Yaakov") == "father" );
+	family::Tree T ("Yosef"); 
+	T.addFather("Yosef", "Yaakov"); 
+	 .addMother("Yosef", "Rachel")   
+	 .addFather("Yaakov", "Isaac")
+	 .addMother("Yaakov", "Rivka")
+	 .addFather("Isaac", "Avraham")
+	 .addFather("Avraham", "Terah");
+	 
+	for (int i = 0; i<100; i++)
+		CHECK( T.relation("Yaakov") == "father" );
 
 }
